f21068f5365e9eb55a352ab9759e0b8b7ce427af
chirani <chirani124@gmail.com>
2025-04-22 11:49:17 +0200

    Adding Drizzle DB

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,9 +1,24 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
 
-export default app
+app.get("/signup", (c) => {
+  return c.text("Hello Hono!");
+});
+
+app.get("/login", (c) => {
+  return c.text("Hello Hono!");
+});
+
+app.get("/logout", (c) => {
+  return c.text("Hello Hono!");
+});
+
+export default {
+  port: 8110,
+  fetch: app.fetch,
+};
