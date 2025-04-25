b419a65ea7d6a26f03ec95cb4a5667bff48b576e
chirani <chirani124@gmail.com>
2025-04-25 19:05:06 +0200

    updated drizzle config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
   out: "./drizzle",
-  schema: "./lib/db/schema.ts",
+  schema: "./packages/server/db/schema.ts",
   dialect: "sqlite",
   dbCredentials: {
     url: process.env.DB_FILE_NAME!,
